67e888b93f91ff1c4fc0681e8ff71af1d0498db2
Nikita Melnikov <nikmel2803@gmail.com>
2022-08-24 16:35:27 +0800

    add docs

--- lib/broker/broker.go
+++ lib/broker/broker.go
@@ -5,25 +5,31 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+// DeliveryChannel represents a channel for receiving messages from a queue
 type DeliveryChannel <-chan amqp.Delivery
 
+// Publisher represents a publisher for publishing messages to a queue
 type Publisher interface {
-	Publish(queueName string, payload []byte) error
+	Publish(queueName string, payload []byte) error // Publish sends a message to a queue
 }
 
+// Broker represents a broker for publishing and receiving messages from a queues
 type Broker interface {
 	Publisher
-	Connect() error
-	Subscribe(queueName string) (DeliveryChannel, error)
-	Close() error
+
+	Connect() error                                      // Connect establishes a connection to the RabbitMQ
+	Subscribe(queueName string) (DeliveryChannel, error) // Subscribe subscribes to a queue and returns a channel for receiving messages
+	Close() error                                        // Close terminates the connection to the RabbitMQ
 }
 
+// RabbitMQ is a broker implementation for RabbitMQ
 type RabbitMQ struct {
 	RabbitmqURL string           // URL for RabbitMQ connection
 	connection  *amqp.Connection // Connection to the RabbitMQ
 	channel     *amqp.Channel    // Channel to the RabbitMQ
 }
 
+// Connect establishes a connection to the RabbitMQ
 func (b *RabbitMQ) Connect() error {
 	var err error
 	b.connection, err = amqp.Dial(b.RabbitmqURL)
@@ -37,6 +43,7 @@ func (b *RabbitMQ) Connect() error {
 	return nil
 }
 
+// Subscribe subscribes to a queue and returns a channel for receiving messages
 func (b *RabbitMQ) Subscribe(queueName string) (DeliveryChannel, error) {
 	receiving, err := b.channel.Consume(
 		queueName, // queue
@@ -50,6 +57,7 @@ func (b *RabbitMQ) Subscribe(queueName string) (DeliveryChannel, error) {
 	return receiving, err
 }
 
+// Publish sends a message to a queue
 func (b *RabbitMQ) Publish(queueName string, payload []byte) error {
 	err := b.channel.PublishWithContext(
 		context.TODO(),
@@ -67,6 +75,7 @@ func (b *RabbitMQ) Publish(queueName string, payload []byte) error {
 	return nil
 }
 
+// Close terminates the connection to the RabbitMQ
 func (b *RabbitMQ) Close() error {
 	if b.channel != nil {
 		err := b.channel.Close()
